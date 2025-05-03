205aaf42b69e9b2fe04cfedd148ac2684b381bd5
mattphillips-one <mattphillips142@gmail.com>
2025-05-02 23:30:30 -0400

    Commit build

--- public/controllers/searchController.js
+++ public/controllers/searchController.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,20 +16,22 @@ exports.SearchController = void 0;
 const bhrrcService_js_1 = __importDefault(require("../services/bhrrcService.js"));
 const filterService_js_1 = __importDefault(require("../services/filterService.js"));
 exports.SearchController = {
-    async search(req, res) {
-        const { query } = req;
-        const searchTerm = (typeof query.query === "string") ? query.query : "";
-        if (searchTerm === "") {
-            res.status(400).send({ message: "Provide a correct search term" });
-        }
-        try {
-            const bhrrc = await bhrrcService_js_1.default.fetchBHRRC(searchTerm);
-            const result = filterService_js_1.default.sortByRecent(bhrrc);
-            res.status(200).json(result);
-        }
-        catch (error) {
-            console.log(error);
-            res.status(500).send({ message: "Guardian service failed" });
-        }
+    search(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { query } = req;
+            const searchTerm = (typeof query.query === "string") ? query.query : "";
+            if (searchTerm === "") {
+                res.status(400).send({ message: "Provide a correct search term" });
+            }
+            try {
+                const bhrrc = yield bhrrcService_js_1.default.fetchBHRRC(searchTerm);
+                const result = filterService_js_1.default.sortByRecent(bhrrc);
+                res.status(200).json(result);
+            }
+            catch (error) {
+                console.log(error);
+                res.status(500).send({ message: "Guardian service failed" });
+            }
+        });
     }
 };
--- public/services/guardianService.js
+++ public/services/guardianService.js
@@ -1,14 +1,25 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    async fetchGuardian(term) {
-        try {
-            const response = await fetch(`https://content.guardianapis.com/search?q=${term}&api-key=test`);
-            const result = await response.json();
-            return result;
-        }
-        catch (error) {
-            throw error;
-        }
+    fetchGuardian(term) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const response = yield fetch(`https://content.guardianapis.com/search?q=${term}&api-key=test`);
+                const result = yield response.json();
+                return result;
+            }
+            catch (error) {
+                throw error;
+            }
+        });
     }
 };
